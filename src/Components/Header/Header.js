6ba23ef11a96e8e879381971eb601aab36d53565
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2021-04-21 10:39:35 +0700

    feat: top header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,28 @@
 import React from "react";
 import map from "lodash/map";
+import { Link } from "react-router-dom";
 
 import Dropdown from "../Dropdown/Dropdown";
 import { dropdownData } from "../../Data/Data";
 
+import logo from "../../Assets/logo.png";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       {/* Header-Top */}
+      <div className="header__top">
+        <a href="/">
+          <img src={logo} alt="" />
+        </a>
+        <div className="header__topMenu">
+          <Link to="/login">Sign In</Link>
+        </div>
+        <div className="header__topMenu">
+          <Link to="/contact-us">Contact Us</Link>
+        </div>
+      </div>
       {/* Dropdown */}
       <div className="header__links">
         {map(dropdownData, (data, key) => {
